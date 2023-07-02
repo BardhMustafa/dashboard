a8e2c29ee7972261b26bac4cc1fa51927a8b3733
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2023-07-02 20:35:59 +0200

    setup eslint and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '@/index.css';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from 'react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
           <App />
+          <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </React.StrictMode>
 );
