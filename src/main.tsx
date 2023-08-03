e8b0d5fbab9334b4719e82c30e40463afa775bae
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2023-08-03 13:51:04 +0200

    remove unnecessary import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from 'react-query-devtools';
 import { RouterProvider } from 'react-router';
 
 import { router } from './routes/router';
