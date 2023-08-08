79a4884b4112f18e96875e889e6061bf0ef3f58a
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2023-08-08 11:38:36 +0200

    fix the logic on private and protected routes

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,9 +1,10 @@
 import { IProtectedProps } from '@/shared/types/routes';
 import { userStore } from '@/store/store';
 import { Navigate } from 'react-router';
+
 const Protected = ({ children }: IProtectedProps) => {
   const { isAuthenticated } = userStore();
-  if (!isAuthenticated) {
+  if (isAuthenticated) {
     return <Navigate to="/" replace />;
   }
   return children;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,11 @@
 import App from '@/App';
 import { createBrowserRouter } from 'react-router-dom';
 import Login from '@/pages/auth/Login';
-import Protected from './Protected';
+import Protected from './ProtectedRoute';
 import Profile from '@/pages/profile/Profile';
 import NotFound from '@/pages/NotFound';
 import Articles from '@/pages/articles/Articles';
+import PrivateRoute from './PrivateRoute';
 
 export const router = createBrowserRouter([
   {
@@ -17,14 +18,19 @@ export const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <Login />,
+
+    element: (
+      <Protected>
+        <Login />
+      </Protected>
+    ),
   },
   {
     path: '/dashboard',
     element: (
-      <Protected>
+      <PrivateRoute>
         <Profile />
-      </Protected>
+      </PrivateRoute>
     ),
   },
   {
